b1d94a191ae867875cdb1a4cff6bef47f2d5f286
prithis <prithisbanerjee783@gmail.com>
2026-05-03 17:21:40 +0530

    Add massage.py to chatllm directory

---
+++ chatllm/massage.py
@@ -0,0 +1,21 @@
+from langchain_core.messages import HumanMessage,SystemMessage,AIMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from dotenv import load_dotenv
+
+load_dotenv()
+model = ChatHuggingFace(llm=HuggingFacePipeline.from_model_id(
+    model_id="Qwen/Qwen3-0.6B",
+    task="text-generation",
+    pipeline_kwargs=dict(temperature=0.5,
+        max_new_tokens=100,
+)))
+
+massages= [
+
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="tell me about langchain?"),
+]
+result = model.invoke(massages)
+
+massages.append(AIMessage(content=result.content))
+print(massages)
